cc9e529a6c9bbba2e8e3aa5f3ad6c9dc4c5d1d2d
Samlv9 <Samlv9@live.com>
2019-04-28 21:42:27 +0800

    增加几个常用的数学常量。

---
+++ src/core/math/index.ts
@@ -0,0 +1,8 @@
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/// @Copyright ~2019 ☜Samlv9☞ and other contributors
+/// @MIT-LICENSE | 3.0.0 | https://developers.guless.com/
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+export const PI2: number = Math.PI * 2;
+export const PI1_2: number = Math.PI * 0.5;
+export const RADIANS_TO_DEGREES: number = 180 / Math.PI;
+export const DEGREES_TO_RADIANS: number = Math.PI / 180;
