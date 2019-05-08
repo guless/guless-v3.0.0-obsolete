cb35f4227bcc19362cd15d109e68466807a9f7b6
Samlv9 <samlv9@live.com>
2019-05-08 17:37:55 +0800

    修复：删除 AssertionError 对象，以便更好的支持 ES5。

--- src/core/assert.ts
+++ src/core/assert.ts
@@ -2,10 +2,8 @@
 /// @Copyright ~2019 ☜Samlv9☞ and other contributors
 /// @MIT-LICENSE | 3.0.0 | https://developers.guless.com/
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-import AssertionError from "./errors/AssertionError";
-
 function assert(condition: boolean, message: string = "assert failed!"): void {
-    if (!condition) { throw new AssertionError(message); }
+    if (!condition) { throw new Error(message); }
 }
 
 export default assert;
