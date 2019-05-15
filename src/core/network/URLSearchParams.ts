69969cf1d7ad76bbf81f2ba01cd02d85652384e8
Samlv9 <samlv9@live.com>
2019-05-15 16:15:05 +0800

    设计目录结构。

--- src/core/network/URLSearchParams.ts
+++ src/core/network/URLSearchParams.ts
@@ -2,15 +2,3 @@
 /// @Copyright ~2019 ☜Samlv9☞ and other contributors
 /// @MIT-LICENSE | 3.0.0 | https://developers.guless.com/
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-/**
- * @see https://tools.ietf.org/html/rfc3986
- */
-class URI {
-    private _scheme: string = "";
-    private _authority: string = "";
-    private _path: string = "";
-    private _query: string = "";
-    private _fragment: string = "";
-}
-
-export default URI;
