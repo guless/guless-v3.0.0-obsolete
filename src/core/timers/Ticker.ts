cc031986fcf72c595d65bb47d0170d77a8eefda0
Samlv9 <samlv9@live.com>
2019-05-16 18:31:08 +0800

    修复：优化 Ticker 计时器的一些问题。

--- src/core/timers/Ticker.ts
+++ src/core/timers/Ticker.ts
@@ -9,18 +9,18 @@ import IEventListener from "../events/IEventListener";
 import IEventListenerObject from "../events/IEventListenerObject";
 import IEventListenerOptions from "../events/IEventListenerOptions";
 import ITickerEventMap from "./ITickerEventMap";
-import { default as microtime, TIME_ORIGIN } from "./microtime";
+import microtime from "./microtime";
 import TickerEvent from "./TickerEvent";
 import TickerInterval from "./TickerInterval";
 
 const __RAF__: null | typeof requestAnimationFrame = (typeof requestAnimationFrame === "function" ? requestAnimationFrame : typeof webkitRequestAnimationFrame === "function" ? webkitRequestAnimationFrame : null);
-const __CAF__: null | typeof cancelAnimationFrame  = (typeof cancelAnimationFrame  === "function" ? cancelAnimationFrame  : typeof webkitCancelAnimationFrame  === "function" ? webkitCancelAnimationFrame  : null);
+const __CAF__: null | typeof cancelAnimationFrame  = (typeof cancelAnimationFrame  === "function" ? cancelAnimationFrame  : typeof webkitCancelAnimationFrame === "function" ? webkitCancelAnimationFrame : null);
 
 class Ticker extends EventDispatcher {
     private static __SHARED_TICKER__: null | Ticker = null;
 
     public static readonly MIN_INTERVAL: number = 10;
-    public static readonly SYNC_REPAINT_AVAILABLE: boolean = (!!__RAF__ && !!__CAF__);
+    public static readonly CAN_SYNC_REPAINT: boolean = (!!__RAF__ && !!__CAF__);
 
     public static get shared(): Ticker {
         return (Ticker.__SHARED_TICKER__ || (Ticker.__SHARED_TICKER__ = new Ticker(TickerInterval.SYNC_REPAINT)));
@@ -29,9 +29,7 @@ class Ticker extends EventDispatcher {
     private _handle?: any;
     private _running: boolean = false;
     private _interval: number;
-    private _preferredSyncRepaintInterval: number = TickerInterval.HIGH;
-    private _onRafUpdate: FrameRequestCallback = (time: number): void => this._onUpdate(TIME_ORIGIN + time);
-    private _onTimerUpdate: FrameRequestCallback = (time: number): void => this._onUpdate(time);
+    private _compatibleSyncRepaintInterval: number = TickerInterval.HIGH;
     
     constructor(interval: number = TickerInterval.SYNC_REPAINT) {
         super();
@@ -59,13 +57,13 @@ class Ticker extends EventDispatcher {
         super.removeEventListener(type, listener, options);
     }
     
-    public setPreferredSyncRepaintInterval(value: number): void {
-        assert(value >= Ticker.MIN_INTERVAL, `Ticker's "preferredSyncRepaintInterval" must great than or equals "MIN_INTERVAL(${Ticker.MIN_INTERVAL}ms)".`);
-        this._preferredSyncRepaintInterval = value;
+    public setCompatibleSyncRepaintInterval(value: number): void {
+        assert(value >= Ticker.MIN_INTERVAL, `Ticker's "compatibleSyncRepaintInterval" must great than or equals "MIN_INTERVAL(${Ticker.MIN_INTERVAL}ms)".`);
+        this._compatibleSyncRepaintInterval = value;
     }
     
-    public getPreferredSyncRepaintInterval(): number {
-        return this._preferredSyncRepaintInterval;
+    public getCompatibleSyncRepaintInterval(): number {
+        return this._compatibleSyncRepaintInterval;
     }
     
     public start(): void {
@@ -82,6 +80,9 @@ class Ticker extends EventDispatcher {
         this._cancelAnimationFrame();
     }
     
+    private _onRafUpdate: FrameRequestCallback = (time: number): void => this._onUpdate(microtime(time));
+    private _onTimerUpdate: FrameRequestCallback = (time: number): void => this._onUpdate(time);
+
     private _onUpdate(time: number): void {
         /**
          * - 1, 由于系统可能调整时钟(Wall Clock)等原因，这里取到的 time 值有可能会小于上一次的 time 值。
@@ -105,28 +106,28 @@ class Ticker extends EventDispatcher {
     }
     
     private _requestAnimationFrame(): any {
-        if (this.interval === TickerInterval.SYNC_REPAINT && Ticker.SYNC_REPAINT_AVAILABLE) {
-            return (__RAF__ as typeof requestAnimationFrame)(this._onRafUpdate);
+        if (this.interval === TickerInterval.SYNC_REPAINT && (__RAF__ && __CAF__)) {
+            return __RAF__(this._onRafUpdate);
         }
         
-        const interval: number = (this._interval === TickerInterval.SYNC_REPAINT ? this._preferredSyncRepaintInterval : this._interval);
+        const timeInterval: number = (this._interval === TickerInterval.SYNC_REPAINT ? this._compatibleSyncRepaintInterval : this._interval);
         const timeCurrent: number = microtime();
-        const timeToCall: number = interval - (timeCurrent % interval);
+        const timeToCall: number = timeInterval - (timeCurrent % timeInterval);
         
         /**
          * 如果本次调用的延时小于最小间隔(MIN_INTERVAL)的话，将回调函数放到下一帧调用。
          * @see [Reasons_for_delays_longer_than_specified](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout#Reasons_for_delays_longer_than_specified)
          */
         if (timeToCall < Ticker.MIN_INTERVAL) {
-            return setTimeout(this._onTimerUpdate, timeToCall + interval, timeToCall + interval + timeCurrent);
+            return setTimeout(this._onTimerUpdate, timeToCall + timeInterval, timeToCall + timeInterval + timeCurrent);
         }
         
         return setTimeout(this._onTimerUpdate, timeToCall, timeToCall + timeCurrent);
     }
     
     private _cancelAnimationFrame(): void {
-        if (this._interval === TickerInterval.SYNC_REPAINT && Ticker.SYNC_REPAINT_AVAILABLE) {
-            return (__CAF__ as typeof cancelAnimationFrame)(this._handle);
+        if (this._interval === TickerInterval.SYNC_REPAINT && (__RAF__ && __CAF__)) {
+            return __CAF__(this._handle);
         }
         
         return clearTimeout(this._handle);
--- src/core/timers/microtime.ts
+++ src/core/timers/microtime.ts
@@ -2,12 +2,15 @@
 /// @Copyright ~2019 ☜Samlv9☞ and other contributors
 /// @MIT-LICENSE | 3.0.0 | https://developers.guless.com/
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const __PERF_NOW_SUPPORTED__: boolean = (typeof performance !== "undefined" && typeof performance.now === "function");
-const __TIME_ORIGIN__: number = (__PERF_NOW_SUPPORTED__ ? Date.now() - performance.now() : 0);
+const __PERF_NOW__: boolean = (typeof performance !== "undefined" && typeof performance.now === "function");
+const __TIME_ORIGIN__: number = (__PERF_NOW__ ? Date.now() - performance.now() : NaN);
 
-function microtime(): number {
-    return (__PERF_NOW_SUPPORTED__ ? __TIME_ORIGIN__ + performance.now() : Date.now());
+function microtime(raftime?: number): number {
+    if (__PERF_NOW__) {
+        return (raftime === void 0 ? __TIME_ORIGIN__ + performance.now() : __TIME_ORIGIN__ + raftime);
+    }
+
+    return Date.now();
 }
 
-export const TIME_ORIGIN: number = __TIME_ORIGIN__;
 export default microtime;
